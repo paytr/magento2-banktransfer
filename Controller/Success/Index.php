f3c1fcadccdab027e9231681cd606ef1c0f9bf9f
paytr-dev-03 <emre.goc@paytr.com>
2023-05-05 10:36:43 +0300

    Various updates have been made and bugs have been fixed.

--- Controller/Success/Index.php
+++ Controller/Success/Index.php
@@ -11,8 +11,6 @@ use Magento\Framework\Controller\Result\Redirect;
 use Magento\Framework\Controller\ResultInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
 use Magento\Sales\Model\Order;
-use Magento\Sales\Model\Order\Email\Sender\OrderSender;
-use Psr\Log\LoggerInterface;
 
 /**
  * Class Index
@@ -24,23 +22,15 @@ class Index extends \Magento\Framework\App\Action\Action
 
     private SuccessValidator $successValidator;
 
-    private $orderSender;
-
-    private $logger;
-
     public function __construct(
         Context $context,
         OrderRepositoryInterface $orderRepository,
         CheckoutSession $checkoutSession,
-        SuccessValidator $successValidator,
-        OrderSender $orderSender,
-        LoggerInterface $logger
+        SuccessValidator $successValidator
     ) {
         parent::__construct($context);
         $this->checkoutSession = $checkoutSession;
         $this->successValidator = $successValidator;
-        $this->orderSender = $orderSender;
-        $this->logger = $logger;
     }
 
     public function execute()
@@ -51,11 +41,6 @@ class Index extends \Magento\Framework\App\Action\Action
             $order->getState() == null) {
                 $order->setState(Order::STATE_PENDING_PAYMENT);
                 $order->save();
-                try {
-                    $this->orderSender->send($order);
-                } catch (\Throwable $e) {
-                    $this->logger->critical($e);
-                }
         }
         $this->checkoutSession->setLastOrderId($order->getId())
             ->setLastSuccessQuoteId($order->getQuoteId())
