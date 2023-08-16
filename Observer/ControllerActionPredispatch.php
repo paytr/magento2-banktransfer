39d8cdb4ee8319996a244e7f8d06704c293d5f7a
Ege Sarısaç <ege.sarisac@paytr.com>
2023-08-16 14:40:07 +0700

    Various updates have been made and bugs have been fixed.

--- Observer/ControllerActionPredispatch.php
+++ Observer/ControllerActionPredispatch.php
@@ -6,7 +6,6 @@ use Magento\Checkout\Model\Session;
 use Magento\Framework\App\Response\Http;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use Magento\Framework\Exception\LocalizedException;
 use Magento\Sales\Model\Order;
 use Magento\Sales\Model\OrderFactory;
 
@@ -36,6 +35,13 @@ class ControllerActionPredispatch implements ObserverInterface
      */
     protected $urlBuilder;
 
+    /**
+     * ControllerActionPredispatch constructor.
+     *
+     * @param Session      $checkoutSession
+     * @param OrderFactory $orderFactory
+     * @param Http         $redirect
+     */
     public function __construct(
         Session $checkoutSession,
         OrderFactory $orderFactory,
@@ -46,6 +52,9 @@ class ControllerActionPredispatch implements ObserverInterface
         $this->_redirect = $redirect;
     }
 
+    /**
+     * @param Observer $observer
+     */
     public function execute(Observer $observer)
     {
         $request = $observer->getData('request');
@@ -53,7 +62,22 @@ class ControllerActionPredispatch implements ObserverInterface
             $orderId = $this->checkoutSession->getLastOrderId();
             if ($orderId) {
                 $order = $this->orderFactory->create()->load($orderId);
-                if (($order->getPayment()->getMethodInstance()->getCode()== "paytr_iframe_transfer") and $order->getState()== Order::STATE_NEW) {
+                if (($order->getPayment()->getMethodInstance()->getCode()== "paytr_iframe_transfer"
+                    ) && $order->getState()== Order::STATE_NEW) {
+                    // refill cart
+                    $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+                    $logger = $objectManager->get('Psr\Log\LoggerInterface');
+                    $cart = $objectManager->get('Magento\Checkout\Model\Cart');
+                    $items = $order->getItemsCollection();
+                    foreach ($items as $item) {
+                        try {
+                            $cart->addOrderItem($item);
+                        } catch (\Exception $e) {
+                            $logger->critical($e);
+                        }
+                    }
+                    $cart->save();
+
                     $this->urlBuilder = \Magento\Framework\App\ObjectManager::getInstance()->get('Magento\Framework\UrlInterface');
                     $url = $this->urlBuilder->getUrl("paytrtransfer/redirect");
                     $this->_redirect->setRedirect($url);
